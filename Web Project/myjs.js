fd356d959bca1e98bc169e961985ecbf2007d466
Zharkyn20 <61348606+Zharkyn20@users.noreply.github.com>
2020-10-29 12:58:40 +0600

    Create myjs.js

---
+++ Web Project/myjs.js
@@ -0,0 +1,31 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function openContacts() {
+  document.getElementById("contacts").style.display = "block";
+}
+
+function closeContacts() {
+  document.getElementById("contacts").style.display = "none";
+}
+
+function openTable() {
+  document.getElementById("about").style.display = "block";
+}
+
+function closeTable() {
+  document.getElementById("about").style.display = "none";
+}
+
+function openPrep() {
+  document.getElementById("prep").style.display = "block";
+}
+
+function closePrep() {
+  document.getElementById("prep").style.display = "none";
+}
